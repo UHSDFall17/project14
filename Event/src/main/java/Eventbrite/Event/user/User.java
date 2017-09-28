a597af963d9eef63bedc85c7fd56b138d1618cda
jeremyle1 <31718351+jeremyle1@users.noreply.github.com>
2017-09-28 13:07:01 -0500

    added user class

---
+++ Event/src/main/java/Eventbrite/Event/user/User.java
@@ -0,0 +1,78 @@
+package eventbrite.user;
+
+public class User {
+	private String firstName, lastName, username, password, email;
+	private Boolean adminRights;
+	
+	public User()
+	{
+		
+	}
+	
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	
+	public String getLastName()
+	{
+		return lastName;
+	}
+	
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
+	
+	public String getEmail()
+	{
+		return email;
+	}
+	
+	public Boolean getAdminRights()
+	{
+		return adminRights;
+	}
+	
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+	
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	
+	public void setPassword(String password)
+	{
+		//password = encryptPassword();
+		this.password = password;
+	}
+	
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+	
+	public void setAdminRights(Boolean adminRights)
+	{
+		this.adminRights = adminRights;
+	}
+	
+//	private void encryptPassword()
+//	{
+//		
+//	}
+
+}
