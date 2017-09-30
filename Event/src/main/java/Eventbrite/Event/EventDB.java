7e5646326e8e27561cd5477a887b8a4b0ba9d283
Jeremy Le <jeremy_le1@yahoo.com>
2017-09-30 03:06:27 -0500

    added initial database class

---
+++ Event/src/main/java/Eventbrite/Event/EventDB.java
@@ -0,0 +1,66 @@
+package Eventbrite.Event;
+
+import java.sql.*;
+
+public class EventDB {
+	static private String connectionString = "jdbc:mysql://localhost:3306/eventbritedb";
+	// Needed to establish a connection with the MySQL server.
+	static private Connection con;
+	// Takes SQL statements and executes them.
+	static private Statement st;
+	// Temporarily holds SQL statements to pass to st.
+	static private String query;
+	// Holds the results of queries.
+	static private ResultSet results;
+	
+	private EventDB()
+	{
+	}
+	
+	public static void getConnection()
+	{
+		try 
+		{
+			// Establish connection to MySQL server.
+			 con = DriverManager.getConnection(connectionString,"root","root");
+			 System.out.println("Connection successful.");
+			 // Create a Statement object which holds SQL statements. 
+			 st = con.createStatement();
+		}
+		catch(Exception e)
+		{
+			System.out.println("EXCEPTION: " + e.getMessage());
+		}
+	}
+	
+	public static void closeConnection()
+	{
+		try {
+			con.close();
+			st.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("EXCEPTION: " + e.getMessage());
+		}
+	}
+	
+	public static void addUser(String username, String firstName,
+			String lastName, String password, String email)
+	{
+		// SQL statements
+		query = "INSERT INTO user"
+				+ " VALUES ('" + username + "','" + firstName + "','" + lastName + "','" + password +
+				"','" + email + "')";
+		try 
+		{
+			// Execute SQL statements in the MySQL server.
+			st.executeUpdate(query);
+		}
+		catch(Exception e)
+		{
+			System.out.println("EXCEPTION: " + e.getMessage());
+		}
+	}
+
+}
