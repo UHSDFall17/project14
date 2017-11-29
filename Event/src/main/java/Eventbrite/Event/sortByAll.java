f49a5c6021725d56d0e7cc3a7df32949b70a943d
JeremyFairchild <31667377+JeremyFairchild@users.noreply.github.com>
2017-11-29 17:54:51 -0600

    Update sortByAll.java
    adds method for showing all created events

--- Event/src/main/java/Eventbrite/Event/sortByAll.java
+++ Event/src/main/java/Eventbrite/Event/sortByAll.java
@@ -30,6 +30,39 @@ public class sortByAll
 		}
 	
 	}
+	public static void allEvents()
+	{
+		System.out.println("You are viewing all events");
+		ArrayList<Event> eventsName;
+		try {
+			eventsName = EventDB.getEvents();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			System.out.println("===========================================");
+			for(Event e : eventsName) {
+				System.out.print("The id for this event is: ");
+				System.out.println(e.getEventID());
+				System.out.print("Event Name: ");
+				System.out.println(e.getEventName());
+				System.out.print("Event Type: ");
+				System.out.println(e.getEventType());
+				System.out.print("Description: ");
+				System.out.println(e.getDescription());
+				System.out.print("Capacity: ");
+				System.out.println(e.getCapacity());
+				System.out.print("Host of Event: ");
+				System.out.println(e.getHostName());
+				System.out.print("Location: ");
+				System.out.println(e.getLocation());
+				System.out.print("Start Date: ");
+				System.out.println(sdf.format(e.getStartDateTime().getTime()));
+				System.out.print("Ticket Price: $");
+				System.out.println(e.getTicketPrice());
+				System.out.println("===========================================");
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+	}
 	
 	
 	
