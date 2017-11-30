df8ba7686c2d67bff7f03b2310146ae25deb8da0
JeremyFairchild <31667377+JeremyFairchild@users.noreply.github.com>
2017-11-29 18:10:06 -0600

    Add files via upload
    Can sort events by location

---
+++ Event/src/main/java/Eventbrite/Event/sortByLocation.java
@@ -0,0 +1,40 @@
+package Eventbrite.Event;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+public class sortByLocation 
+{
+	public static void sortLocation()
+	{
+		System.out.println("Sorting by Price.");
+		ArrayList<Event> eventsName;
+		try {
+			eventsName = EventDB. getEventsSortedByLocation();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			System.out.println("===========================================");
+			for(Event e : eventsName) {
+				System.out.print("The id for this event is: ");
+				System.out.println(e.getEventID());
+				System.out.print("Event Name: ");
+				System.out.println(e.getEventName());
+				System.out.print("Event Type: ");
+				System.out.println(e.getEventType());
+				System.out.print("Description: ");
+				System.out.println(e.getDescription());
+				System.out.print("Capacity: ");
+				System.out.println(e.getCapacity());
+				System.out.print("Host of Event: ");
+				System.out.println(e.getHostName());
+				System.out.print("Location: ");
+				System.out.println(e.getLocation());
+				System.out.print("Start Date: ");
+				System.out.println(sdf.format(e.getStartDateTime().getTime()));
+				System.out.print("Ticket Price: $");
+				System.out.println(e.getTicketPrice());
+				System.out.println("===========================================");
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}	
+	}
+}
