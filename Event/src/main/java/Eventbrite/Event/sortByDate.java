6c4cfc622890ae47b87cc1a8ff2b627cedf5f287
JeremyFairchild <31667377+JeremyFairchild@users.noreply.github.com>
2017-11-29 18:11:18 -0600

    added sortByDate class

---
+++ Event/src/main/java/Eventbrite/Event/sortByDate.java
@@ -0,0 +1,40 @@
+package Eventbrite.Event;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+public class sortByDate 
+{
+	public static void sortDate()
+	{
+		System.out.println("Sorting by Price.");
+		ArrayList<Event> eventsName;
+		try {
+			eventsName = EventDB. getEventsSortedByDate();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			System.out.println("===========================================");
+			for(Event e : eventsName) {
+				System.out.print("The id for this event is: ");
+				System.out.println(e.getEventID());
+				System.out.print("Event Name: ");
+				System.out.println(e.getEventName());
+				System.out.print("Event Type: ");
+				System.out.println(e.getEventType());
+				System.out.print("Description: ");
+				System.out.println(e.getDescription());
+				System.out.print("Capacity: ");
+				System.out.println(e.getCapacity());
+				System.out.print("Host of Event: ");
+				System.out.println(e.getHostName());
+				System.out.print("Location: ");
+				System.out.println(e.getLocation());
+				System.out.print("Start Date: ");
+				System.out.println(sdf.format(e.getStartDateTime().getTime()));
+				System.out.print("Ticket Price: $");
+				System.out.println(e.getTicketPrice());
+				System.out.println("===========================================");
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}		
+	}
+}
